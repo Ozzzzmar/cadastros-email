8e8738e8ad7a0fe0610eeb67ccd1e59a91eabf60
Osmar <juninhomachado579@gmail.com>
2024-06-17 10:41:52 -0500

    feat: pequena alteração

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ console.log("Olá, por favor, digite o seu e-mail");
 
 process.stdin.on("data", function (data) {
     let entrada = data.toString().trim();
-    let email = false;
+    // let email = false;
+    let usuario;
 
     for(let i = 0; i < cadastros.length; i++){
         if(entrada == cadastros[i].email){
-            email = true;
+            // email = true;
+            usuario = cadastros[i];
         }
     }
 
-    if(email){
-        console.log("Seja Bem Vindo!");
+    if(usuario){
+        console.log("Seja Bem Vindo(a) " + usuario.nome + "!");
     }else{
         console.log("Você não está cadastrado!");
     }
