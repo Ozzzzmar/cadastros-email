3eb4a3fdcb3bde2486644d006755e47adc6a2876
Osmar <juninhomachado579@gmail.com>
2024-06-17 09:27:13 -0500

    feat: verificação de usuário

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const cadastros = [
   {nome: "Bruno", idade: "24", email: "brunoreidelas345@gmail.com"},
   {nome: "Cleiton", idade: "48", email: "cleitindograu2@hotmail.com"},
-  {nome: "Linda", idade: "18", email: "feia123@gmail.com"}
+  {nome: "Linda", idade: "18", email: "feia123@outlook.com"}
 ];
 
+console.log("Olá, por favor, digite o seu e-mail");
+
+process.stdin.on("data", function (data) {
+    let entrada = data.toString().trim();
+
+    for(let i = 0; i < cadastros.length; i++){
+        if(entrada == cadastros[i].email){
+            console.log("Seja Bem Vindo!");
+        }else{
+            console.log("Você não está cadastrado!");
+        }
+    }
+});
