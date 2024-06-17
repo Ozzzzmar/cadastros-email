e913bfe3792fa2342f98c52ce2a100808d9d428f
Osmar <juninhomachado579@gmail.com>
2024-06-17 08:54:39 -0500

    feat: array de objetos

---
+++ index.js
@@ -0,0 +1,6 @@
+const cadastros = [
+  {nome: "Bruno", idade: "24", email: "brunoreidelas345@gmail.com"},
+  {nome: "Cleiton", idade: "48", email: "cleitindograu2@hotmail.com"},
+  {nome: "Linda", idade: "18", email: "feia123@gmail.com"}
+];
+
